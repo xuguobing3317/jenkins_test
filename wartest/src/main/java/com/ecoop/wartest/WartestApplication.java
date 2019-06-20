c9a23e392cdd242fff554655f9c0da6e3c9ada85
crazy-xu <xuguobing3317@163.com>
2019-06-20 20:15:13 +0800

    war提交

---
+++ wartest/src/main/java/com/ecoop/wartest/WartestApplication.java
@@ -0,0 +1,13 @@
+package com.ecoop.wartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WartestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WartestApplication.class, args);
+    }
+
+}
